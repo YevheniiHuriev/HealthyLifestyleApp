adb390ac6b6ccb23e4ddeb54d50caa8e8d047284
Oleksandr Zolotopup <142610556+OleksDragon@users.noreply.github.com>
2025-08-28 17:57:56 +0300

    Implementation of animated background with balls (#22)

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -5,13 +5,19 @@ import ContentSection from "../landing-page/content-section/ContentSection";
 import MarketplaceSection from "../landing-page/marketplace-section/MarketplaceSection";
 import FAQSection from "../landing-page/faq-section/FAQSection";
 import Footer from "../landing-page/footer/Footer";
+import Bubble from "../landing-page/animated-background/Bubble";
 
 function HomePage() {
-
   return (
     <div>
       <MainSection />
+      <div className="bubbles-container between-sections">
+        <Bubble className="bubble-2" />
+      </div>
       <MainContentBlock />
+      <div className="bubbles-container between-sections">
+        <Bubble className="bubble-4" />
+      </div>
       <ContentSection />
       <MarketplaceSection />
       <FAQSection />
