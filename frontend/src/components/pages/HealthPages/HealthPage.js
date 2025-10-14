f54df87b8194f330cc0192f236a45d7c4aa05384
Yevhenii Huriev <142521006+YevheniiHuriev@users.noreply.github.com>
2025-10-14 07:46:33 +0300

    add stub animation (#48)

--- frontend/src/components/pages/HealthPages/HealthPage.js
+++ frontend/src/components/pages/HealthPages/HealthPage.js
@@ -1,8 +1,33 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import health_nomi from "../../../assets/animation/health_nomi.json"
+import health_n_text from "../../../assets/animation/health_n_text.json"
+import "../../styles/health.css"
+
 const HealthPages = () => {
     return(
-        <>
-        
-        </>
+        <div className="hp-health-container">
+            <Lottie 
+                animationData={health_nomi}
+                loop={true}
+                autoplay={true}
+                className="hp-health-animation-element-nomi"
+                style={{
+                    width: 700,
+                    height: 550
+                }}
+            />
+            <Lottie 
+                animationData={health_n_text}
+                loop={false}
+                autoplay={true}
+                className="hp-health-animation-element-text"
+                style={{
+                    width: 500,
+                    height: 400
+                }}
+            />
+        </div>
     );
 }
 
